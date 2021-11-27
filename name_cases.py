d97240a7759df0592e9165cef75da57f36a6107f
Sergio Pedro <seperool@gmail.com>
2021-11-27 19:56:53 -0300

    funções str

---
+++ name_cases.py
@@ -0,0 +1,7 @@
+name = "sergio pedro "
+name = name.title()
+name = name.strip()
+print ('Olá '+name+"! "+"\n\tGostaria de aprender um pouco de Python hoje?")
+print (name.lower())
+print (name.upper())
+print (name.title())
