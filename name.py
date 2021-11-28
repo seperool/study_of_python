c85cc16f48b2197f38d5f55c5944fee83e975e14
Sergio Pedro <seperool@gmail.com>
2021-11-28 13:36:00 -0300

    variavel str & funções str

---
+++ name.py
@@ -0,0 +1,14 @@
+nome = "ada lovelace"
+print (nome.title(),"title")
+print (nome.upper(),"upper")
+print (nome.lower(),"lower")
+
+fist_name= "sergio"
+last_name = "oliveira"
+full_name = fist_name+" "+last_name
+print(full_name)
+
+print("Hello, \n"+full_name.title()+"!")
+
+nome_2= "ana rodrigues "
+print(nome_2.rstrip()+"hello!")
