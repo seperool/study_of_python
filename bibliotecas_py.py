2865548645e8c5feff59c93be2a9c71bf86ec669
seperool <seperool@gmail.com>
2024-03-30 23:30:03 -0300

    Principais bibliotecas de python

---
+++ bibliotecas_py.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 23:07:22 2024
+
+@author: Sérgio
+"""
+
+import os
+import math
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import kivy
