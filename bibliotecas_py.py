19cda0d92417802fc4609b9855d627ef80e3a4bb
seperool <seperool@gmail.com>
2024-07-31 18:39:15 -0300

    bibliotecas de quarto e comentarios sobre bibliotecas

--- bibliotecas_py.py
+++ bibliotecas_py.py
@@ -5,8 +5,8 @@ Created on Sat Mar 30 23:07:22 2024
 @author: Sérgio
 """
 
-import os
-import math
+import os #comandos OS
+import math #Funções matematicas (?)
 import numpy as np
 import pandas as pd
 
@@ -18,3 +18,7 @@ import sklearn
 import matplotlib.pyplot as plt
 import seaborn as sns
 import kivy
+
+#Quarto
+from IPython.display import Markdown
+from tabulate import tabulate
