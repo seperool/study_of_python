4ed839f7d5118879f0dd80cac446fb91033e762f
seperool <seperool@gmail.com>
2024-08-02 13:19:34 -0300

    Editando strings

--- Curso_Python/cap1-name.py
+++ Curso_Python/cap1-name.py
@@ -5,15 +5,18 @@ Created on Wed Jul 24 15:51:13 2024
 
 @author: sergio
 """
+# print
 
 name = "ada lovelace"
 print(name.title()) #Primeiras letras em maiusculas
 print(name.upper()) #Tudo em maiuscula
 print(name.lower()) #Tudo em minuscula
 
-#Quando for armazenar uma informação é util transformar em minuscula.
+##Quando for armazenar uma informação é util transformar em minuscula.
 
-#Concatenação
+#-----------------------------------------------------------------------------#
+
+##Concatenação
 #1)
 first_name = "ada"
 last_name = "lovelace"
@@ -26,3 +29,27 @@ print("Hello, " + full_name.title() + "!")
 #3)
 message = "Hello, " + full_name.title() + "!"
 print(message)
+
+#-----------------------------------------------------------------------------#
+
+## removendo espaços em branco
+
+favorite_linguage = ' python '
+
+#1) Removendo espaços em branco do lado direito
+
+favorite_linguage.rstrip()
+
+#2) Removendo espaços em branco do lado esquerdo
+
+favorite_linguage.lstrip()
+
+#3) Removendo espaços em branco dos dois lados
+
+favorite_linguage.strip()
+
+#4) Armazenando num variável
+# Remover espaços em branco não armazena dados automaticamente.
+
+favorite_linguage = favorite_linguage.strip()
+print(favorite_linguage)
