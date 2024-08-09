c9767b54d7620a2047f5aa9ffa6254155787ed23
seperool <seperool@gmail.com>
2024-08-09 14:19:32 -0300

    Organizando estudo de python

---
+++ Curso_Python/Bibliotecas_python/bibliotecas_py.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 23:07:22 2024
+
+@author: Sérgio
+"""
+
+import os #comandos OS
+import math #Amplia as funções matematicas (básicas, trigonométricas, estatísticas)
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import kivy
+
+#Quarto
+from IPython.display import Markdown
+from tabulate import tabulate
---
+++ Curso_Python/Cap03-Listas/cap03-listas_1_a_3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 13:52:35 2024
+
+@author: sergio
+"""
+#Exercicios sobre listas:
+
+#1)
+nomes = ["Alexandre","Gabriela","Patrick"]
+print(nomes[0])
+print(nomes[1])
+print(nomes[2])
+
+#2)
+print("Olá, " + nomes[0] + " saudações!")
+print("Olá, " + nomes[1] + " saudações!")
+print("Olá, " + nomes[2] + " saudações!")
+
+#3)
+motorcycles = ['honda','yamaha','suzuki','ducati']
+print("Gostaria de ter uma moto " + motorcycles[0].title())
