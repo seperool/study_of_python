df91cea7d029ec5fea05ea6d5a6f36064918d01e
seperool <seperool@gmail.com>
2024-08-12 14:29:01 -0300

    Exercicios do capitulo 3 - 8

---
+++ Cap03-Listas/cap03-ordenar_listas_8_a_10.py
@@ -0,0 +1,23 @@
+#8)
+mundo = ['cuba','russia','japao','mexico','argentina']
+print(mundo) #Ordem original
+print(sorted(mundo)) #Ordem alfabética temporaria
+print(mundo) #Ordem oringal
+print(sorted(mundo,reverse=True)) #Ordem alfabética inversa temporaria
+print(mundo) #Ordem oringal
+
+# Ordem inversa
+mundo.reverse() #Invertendo lista
+print(mundo)
+mundo.reverse() #invertendo lista invertida, voltando a ordem original
+print(mundo)
+
+# Ordem alfabética
+mundo.sort() #Ordem alfabética
+print(mundo)
+mundo.sort(reverse=True) #Ordem alfabética inversa
+print(mundo)
+
+#9)
+
+#10)
