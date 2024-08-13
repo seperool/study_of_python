bdcb3fec95fca6311e8efa37bdde426f2adddde4
seperool <seperool@gmail.com>
2024-08-13 18:05:37 -0300

    Exemplos de programas usando listas

---
+++ Cap04-Trabalhando_com_listas/pizzas.py
@@ -0,0 +1,12 @@
+#1)
+pizzas = ["mussarela","pepperoni","calabresa"]
+for pizza in pizzas:
+    print("Gosto de pizza de " + pizza + ".\n")
+print("Eu realmente gosto de pizza!")
+
+#2)
+animais = ["cachorro","gato","tartaruga"]
+for animal in animais:
+    print("Um(a) " + animal + " seria um ótimo animal de estimação.")
+print("Todos esses animais tem quarto patas.")
+print("Qualquer um desses animais seria um ótimo animal de estimação!")
