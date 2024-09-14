bc3815165693b8e4d3bf5615ae07abeebf99afee
seperool <seperool@gmail.com>
2024-09-14 00:19:16 -0300

    Exercicios 7 a 12 do capitulo 6 - dicionários em python

--- Cap06-Dicionarios/Exercicios_7_a_12.py
+++ Cap06-Dicionarios/Exercicios_7_a_12.py
@@ -32,8 +32,48 @@ for pet in pets:
 
 #9)
 
-
+favorite_places = {
+    'sergio': ['nilopolis','nova friburgo'],
+    'maria': ['bangu'],
+    'marcelo': ['araruama','saquarema','rio das ostras'],
+    'eduardo':['nova iguaçu','rio das ostras']
+    }
+for name, places in favorite_places.items():
+    if len(places) != 1:
+        print("\nOs lugares favoritos de " + name.title() + " são:")
+        for place in places:
+            print(place.title())
+    else:
+        print("\nO lugar favorito de " + name.title() + " é " + places[0].title())
 
 #10)
+
+number_faverite = {'mario':[42,56],
+                   'ricardo':[54,38],
+                   'sergio':[29,48],
+                   'marcela':[96,100],
+                   'rafaela':[12,1,79]
+                   }
+for nome, numeros in number_faverite.items():
+    print("\nOs números favoritos de " + nome.title() + " são:")
+    for numero in numeros:
+        print(numero)
+
 #11)
-#12)
+
+cities = {
+    'nova friburgo':{'country':'brasil',
+                     'population':203328,
+                     'id':'nf'},
+    'nilopolis':{'country':'brasil',
+                 'population':16000,
+                 'id':'n'},
+    'nova iguaçu':{'country':'brasil',
+                   'population':843046,
+                   'id':'ni'}
+    }
+
+for cidade, infos in cities.items():
+    print("\nA cidade de " + cidade.title() + " tem:")
+    for curiosidade, info in infos.items():
+        print(curiosidade.title() + ": " + str(info))
