93af6e97652a9f346809561ff11b08918e5f9292
seperool <seperool@gmail.com>
2024-09-16 02:27:32 -0300

    Exemplo de while

---
+++ Cap07-Entrada_usuario_e_while/counting.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 16 02:03:46 2024
+
+@author: sergio
+"""
+
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
