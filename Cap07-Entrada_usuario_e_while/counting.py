a06c06a2cec24389c2afd9072338553747cb40d6
seperool <seperool@gmail.com>
2024-09-17 04:23:48 -0300

    while continue

--- Cap07-Entrada_usuario_e_while/counting.py
+++ Cap07-Entrada_usuario_e_while/counting.py
@@ -6,7 +6,12 @@ Created on Mon Sep 16 02:03:46 2024
 @author: sergio
 """
 
-current_number = 1
-while current_number <= 5:
-    print(current_number)
+#While continue
+#A instrução 'continue' volta o laço para o início
+
+current_number = 0
+while current_number < 10:
     current_number += 1
+    if (current_number % 2) == 0: #Se for par
+        continue
+    print(current_number)
