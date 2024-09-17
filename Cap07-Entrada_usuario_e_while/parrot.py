dfed30c02f96737a19e2966fa9f7221142ecb232
seperool <seperool@gmail.com>
2024-09-17 03:19:35 -0300

    Exemplo while flag

--- Cap07-Entrada_usuario_e_while/parrot.py
+++ Cap07-Entrada_usuario_e_while/parrot.py
@@ -6,5 +6,16 @@ Created on Sun Sep 15 00:59:24 2024
 @author: sergio
 """
 
-message = input("Tell me soomething, and I will repeat it back to you: ")
-print(message)
+prompt = "\nTell me soomething, and I will repeat it back to you:"
+prompt += "\n(Enter 'quit' to end the program.)"
+prompt += "\n"
+
+active = True
+
+message = ""
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
