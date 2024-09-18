f6c0648e8a325ce190016004251c92445df0fdb5
seperool <seperool@gmail.com>
2024-09-18 17:10:11 -0300

    Exercicio 7 cap 7 - while loop infinito

---
+++ Cap07-Entrada_usuario_e_while/Exercicio_7-infinito.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 18 16:55:51 2024
+
+@author: sergio
+"""
+
+#while infinito
+#'CTRL + C' para sair do loop infinito
+
+x = 1
+while x > 0 :
+    x += 1
+    print(x)
