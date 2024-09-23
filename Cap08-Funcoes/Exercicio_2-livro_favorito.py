5beba3800a3eac520bfc4af914f69fb140dc8e91
seperool <seperool@gmail.com>
2024-09-23 12:23:22 -0300

    Exercicio 2 - parâmetro e argumento para função

---
+++ Cap08-Funcoes/Exercicio_2-livro_favorito.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 12:19:16 2024
+
+@author: sergio
+"""
+
+#Parâmetros e passando argumento para função
+
+def favorite_book (title): #Parâmetro
+    print("Um dos meus livros favoritos é " + title.title() + ".")
+
+favorite_book('a terra da noite') #Argumento
