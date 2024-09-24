bd4b13d8c400a085e6772848744be9ad2ed887bb
seperool <seperool@gmail.com>
2024-09-24 00:01:25 -0300

    Exemplo função - argumentos e parametros

---
+++ Cap08-Funcoes/pets.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 14:57:21 2024
+
+@author: sergio
+"""
+
+def describe_pet(animal_type, pet_name):
+    """Exibe informações sobre um animal de estimação."""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+    
+describe_pet('hamster', "harry") #Argumento posicional
