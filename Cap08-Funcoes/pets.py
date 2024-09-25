eabcc97b74b8847d3dc960af1aecd58b8a7852fa
seperool <seperool@gmail.com>
2024-09-25 14:09:47 -0300

    Revisão e add comentários

--- Cap08-Funcoes/pets.py
+++ Cap08-Funcoes/pets.py
@@ -6,6 +6,12 @@ Created on Mon Sep 23 14:57:21 2024
 @author: sergio
 """
 
+#Passando argumentos para função
+
+#Argumentos posicinais:
+#Os argumentos são passados na mesma ordem 
+#em que os parâmetros na definição da função aparecem
+
 def describe_pet(animal_type, pet_name):
     """Exibe informações sobre um animal de estimação."""
     print("\nI have a " + animal_type + ".")
