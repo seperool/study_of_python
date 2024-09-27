3d0553891156134e9015702555646a99b5e0066a
seperool <seperool@gmail.com>
2024-09-27 16:29:39 -0300

    Passando argumentos - default e argumentos nomeados

--- Cap08-Funcoes/pets.py
+++ Cap08-Funcoes/pets.py
@@ -12,10 +12,12 @@ Created on Mon Sep 23 14:57:21 2024
 #Os argumentos são passados na mesma ordem 
 #em que os parâmetros na definição da função aparecem
 
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name,animal_type='dog'): #Definindo default
     """Exibe informações sobre um animal de estimação."""
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
     
 describe_pet('hamster', "harry") #Argumento posicional
 describe_pet('dog', "willie") #Várias chamadas de função
+describe_pet(animal_type="hamster", pet_name="harry") #Argumentos nomeados
+describe_pet(pet_name="willie")
