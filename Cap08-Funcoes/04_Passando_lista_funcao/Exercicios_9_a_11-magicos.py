f2852490720cfabc6714a6a7d4aba99412d62ec1
seperool <seperool@gmail.com>
2024-10-05 13:49:35 -0300

    Organizando pastas

---
+++ Cap08-Funcoes/04_Passando_lista_funcao/Exercicios_9_a_11-magicos.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  5 13:20:52 2024
+
+@author: sergio
+"""
+
+#Passando uma lista para uma função
+
+def show_magicians(lista_magicos):
+    print("\nLista de todos os magicos:")
+    for magico in lista_magicos:
+        print(magico.title())
+
+def make_great(lista):
+    count = 0
+    while count < len(lista):
+        lista[count] = "Grande " + lista[count]
+        count += 1
+    return lista
+
+lista_magicos = ['david cooperfild', 'cris angel', 'mister m']
+list_great_magicians = make_great(lista_magicos[:]) #Passando uma copia da lista
+show_magicians(lista_magicos)
+show_magicians(list_great_magicians)
