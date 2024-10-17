e50f0ccbcd4f9c22920f89c41501c5f12e214090
seperool <seperool@gmail.com>
2024-10-16 21:58:50 -0300

    Exemplo de uso de as para atribuir alias ao módulo

---
+++ Cap08-Funcoes/06_Modulos/making_pizza-alias_modulo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 21:46:25 2024
+
+@author: sergio
+"""
+
+#Importando um módulo
+#Dando um alias que substitui o nome do módulo
+import pizza as p
+
+#Usando alias do modulo e uma função do módulo
+#Usando a módulo: alias_do_modulo.nome_função()
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushrooms','green peppers','extra cheese')
