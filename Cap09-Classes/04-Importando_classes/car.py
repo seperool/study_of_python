f85deef8b5fba097e2ac2f9c29a924dff56bd57d
seperool <seperool@gmail.com>
2024-11-08 01:11:11 -0300

    Módulo com várias classes

--- Cap09-Classes/04-Importando_classes/car.py
+++ Cap09-Classes/04-Importando_classes/car.py
@@ -6,8 +6,11 @@ Created on Tue Nov  5 12:21:35 2024
 @author: sergio
 """
 
-"""Uma classe que pode ser usada para representar um carro."""
+"""
+Um conjunto de classes usado para representar carros á gasolina e elétricos.
+"""
 
+#Classes
 class Car():
     """Uma tentativa simples de representar um carro."""
     def __init__(self,make,model,year):
@@ -49,3 +52,41 @@ class Car():
         Soma a quantidade especificada ao valor de leitura do hodômetro.
         """
         self.odometer_reading += miles
+
+class Battery():
+    """Uma tentativa simples de modelar uma bateria para um carro elétrico."""
+    
+    #Atributos
+    def __init__(self,battery_size=70):
+        """Inicializa atributos da bateria."""
+        self.battery_size = battery_size
+    
+    #Métodos
+    def describe_battery(self):
+        """Exibe uma fraseque descreve a capacidade da bateria."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+    
+    def get_range(self):
+        """
+        Exibe frase sobre a distância que o carro pode percorrer 
+        com essa bateria.
+        """
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+class ElectricCar(Car):
+    
+    #Atributos
+    def __init__(self,make,model,year):
+        """
+        Inicializa os atributos da classe-pai.
+        Em seguida, inicializa os atributos específicos de um carro elétrico.
+        """
+        super().__init__(make,model,year)
+        self.battery = Battery()
