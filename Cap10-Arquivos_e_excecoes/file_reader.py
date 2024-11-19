df7f02e468b5035db878bb192b080d74b72b9932
seperool <seperool@gmail.com>
2024-11-19 17:47:14 -0300

    Programa para ler um arquivo inteiro

---
+++ Cap10-Arquivos_e_excecoes/file_reader.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 17:18:18 2024
+
+@author: sergio
+"""
+
+#Abre arquivo, lê o conteudo e salva numa variável
+#with fecha o arquivo depois de usa-lo
+#open() abre o arquivo
+#'as' salva o conteudo do arquivo no objeto file_object
+with open('pi_digits.txt') as file_object:
+    #Lê o conteúdo do arquivo, salva como string
+    contents = file_object.read()
+    #Elimina o caracter em branco do final
+    contents = contents.rstrip()
+    #Confere  tipo da variável, resultado esperado string
+    print(type(contents))
+    #Printa o conteúdo da variável = conteúdo do arquivo
+    print(contents)
