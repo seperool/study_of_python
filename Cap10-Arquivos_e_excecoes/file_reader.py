6b3a387d89057af319daff0e18f2b1e4afae992a
seperool <seperool@gmail.com>
2024-11-24 22:53:35 -0300

    Revisão do path de arquivo

--- Cap10-Arquivos_e_excecoes/file_reader.py
+++ Cap10-Arquivos_e_excecoes/file_reader.py
@@ -10,12 +10,17 @@ Created on Tue Nov 19 17:18:18 2024
 #with fecha o arquivo depois de usa-lo
 #open() abre o arquivo
 #'as' salva o conteudo do arquivo no objeto file_object
-with open('pi_digits.txt') as file_object:
+path_text = 'text_files/pi_digits.txt' #path relativo do arquivo, caminho
+with open(path_text) as file_object:
+    
     #Lê o conteúdo do arquivo, salva como string
     contents = file_object.read()
+    
     #Elimina o caracter em branco do final
     contents = contents.rstrip()
+    
     #Confere  tipo da variável, resultado esperado string
     print(type(contents))
+    
     #Printa o conteúdo da variável = conteúdo do arquivo
     print(contents)
