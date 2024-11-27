ee386bc7a284937abd9017a237a34cc311891204
seperool <seperool@gmail.com>
2024-11-27 14:13:27 -0300

    Add comentarios

--- Cap10-Arquivos_e_excecoes/file_read-readlines.py
+++ Cap10-Arquivos_e_excecoes/file_read-readlines.py
@@ -11,7 +11,10 @@ file_name = 'text_files/pi_digits.txt'
 
 #Abrir arquivo pi_digits.txt
 with open(file_name) as file_object:
+    
+    #Método readlines, para leitura de linhas em uma lista
     lines = file_object.readlines()
     
+#Percorrendo a lista de linhas
 for line in lines:
-    print(line.rstrip())
+    print(line.rstrip()) #Elimando espaços em branco com o método rstrip()
