0ade595522b83e8b5f32539a7f2c30cc01a4fb50
seperool <seperool@gmail.com>
2024-11-29 00:41:11 -0300

    Lendo um milhão de digitos

--- Cap10-Arquivos_e_excecoes/pi_string-million.py
+++ Cap10-Arquivos_e_excecoes/pi_string-million.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 28 22:25:10 2024
+Created on Fri Nov 29 00:01:43 2024
 
 @author: sergio
 """
 
 #path, caminho ate o arquivo
-file_name = 'text_files/pi_digits.txt'
+file_name = 'text_files/pi_million_digits.txt'
 
 with open(file_name) as file_object: #Abre arquivo
     lines = file_object.readlines() #Cria um lista de linhas
@@ -16,5 +16,5 @@ pi_string = '' #Inicializa string pi como vazia
 for line in lines: #Pecorre as linhas
     pi_string += line.strip() #Junta strings e remove espaços em branco
 
-print(pi_string) #string pi sem espaço em branco
+print(pi_string[:50] + "...") #string pi sem espaço em branco
 print(len(pi_string)) #Tamanho da string
