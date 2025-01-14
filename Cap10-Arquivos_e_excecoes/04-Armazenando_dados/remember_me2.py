7c2e5bee3a082d16d6e101597dd9d75fe3204319
seperool <seperool@gmail.com>
2025-01-13 22:22:41 -0300

    Refatorando remember_me

--- Cap10-Arquivos_e_excecoes/04-Armazenando_dados/remember_me2.py
+++ Cap10-Arquivos_e_excecoes/04-Armazenando_dados/remember_me2.py
@@ -8,18 +8,31 @@ Created on Sun Jan 12 22:45:16 2025
 
 import json
 
-# Carrega o nome do usuário se foi armazenado anteriormente
-# Caso contrário, pede que o usuário forneça o nome e armazena essa informação
-filename = "username.json"
+def get_stored_username():
+    """Obtém o nome do usuário já armazenado se estiver disponível."""
+    # Carrega o nome do usuário se foi armazenado anteriormente
+    # Caso contrário, pede que o usuário forneça o nome e armazena essa informação
+    filename = "username.json"
 
-try:
-    with open(filename) as f_obj:
-        username = json.load(f_obj)
-except FileNotFoundError:
-    username = input("What is your name? ")
-    with open(filename,'w') as f_obj:
-        json.dump(username,f_obj)
-        print("We'll remember you when you como back, "+
-              username + "!")
-else:
-    print("Wellcome back, " + username + "!")
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def greet_user():
+    """Saúda o usuário pelo nome."""
+    username = get_stored_username()
+    if username:
+        print("Welcome back, " + username + "!")
+    else:
+        username = input("What is your name? ")
+        filename = "username.json"
+        with open(filename,'w') as f_obj:
+            json.dump(username, f_obj)
+            print("We'll remember you when you come back, " + 
+                  username + "!")
+
+greet_user()
