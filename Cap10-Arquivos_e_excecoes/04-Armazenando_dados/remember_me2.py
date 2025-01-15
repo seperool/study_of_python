065b4d808a379efbb2556392d913be28239e3e73
seperool <seperool@gmail.com>
2025-01-14 22:40:48 -0300

    Exemplo de refatoração

--- Cap10-Arquivos_e_excecoes/04-Armazenando_dados/remember_me2.py
+++ Cap10-Arquivos_e_excecoes/04-Armazenando_dados/remember_me2.py
@@ -22,17 +22,22 @@ def get_stored_username():
     else:
         return username
 
+def get_new_username():
+    """Pede um novo nome de usuário."""
+    username = input("What is your name? ")
+    filename = "username.json"
+    with open(filename,'w') as f_obj:
+        json.dump(username, f_obj)
+    return username
+
 def greet_user():
     """Saúda o usuário pelo nome."""
     username = get_stored_username()
     if username:
         print("Welcome back, " + username + "!")
     else:
-        username = input("What is your name? ")
-        filename = "username.json"
-        with open(filename,'w') as f_obj:
-            json.dump(username, f_obj)
-            print("We'll remember you when you come back, " + 
-                  username + "!")
+        username = get_new_username()
+        print("We'll remember you when you come back, " + 
+              username + "!")
 
 greet_user()
