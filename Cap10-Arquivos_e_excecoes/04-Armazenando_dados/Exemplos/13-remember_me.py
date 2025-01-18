443b8d184cded3da9d5da65050c4bcd626db85c0
seperool <seperool@gmail.com>
2025-01-18 00:17:16 -0300

    Exercicio 13

---
+++ Cap10-Arquivos_e_excecoes/04-Armazenando_dados/Exemplos/13-remember_me.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 00:03:30 2025
+
+@author: sergio
+"""
+
+import json
+
+def get_stored_username():
+    """Obtém o nome do usuário já armazenado se estiver disponível."""
+    # Carrega o nome do usuário se foi armazenado anteriormente
+    # Caso contrário, pede que o usuário forneça o nome e armazena essa informação
+    filename = "username.json"
+
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """Pede um novo nome de usuário."""
+    username = input("What is your name? ")
+    filename = "username.json"
+    with open(filename,'w') as f_obj:
+        json.dump(username, f_obj)
+    return username
+
+def greet_user():
+    """Saúda o usuário pelo nome."""
+    username = get_stored_username()
+    if username:
+        resposta = input("you are " + username + "? (n/s) \n")
+        if resposta == 's':
+            print("Welcome back, " + username + "!")
+        else:
+            print("Você não foi o ultimo usuário a se cadastrar.") 
+            resposta2 = input("Gostaria de se cadastrar novamente? (s/n)\n")
+            if resposta2 == 's':
+                username = get_new_username()
+    else:
+        username = get_new_username()
+        print("We'll remember you when you come back, " + 
+              username + "!")
+
+greet_user()
