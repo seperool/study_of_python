0bbc5aa6d54de68885cf881d9d0e0d15c96951bb
seperool <seperool@gmail.com>
2025-01-26 21:53:24 -0300

    Adicionando comentários

--- Cap11-Testando_codigo/01-Testando_funcao/names.py
+++ Cap11-Testando_codigo/01-Testando_funcao/names.py
@@ -6,16 +6,17 @@ Created on Wed Jan 22 21:56:26 2025
 @author: sergio
 """
 
-from name_function import get_formatted_name
+from name_function import get_formatted_name  # Importa função
 
-print("Enter 'q' at any time to quit.")
-while True:
-    first = input("\nPlase give me a first name: ")
-    if first == 'q':
+print("Enter 'q' at any time to quit.")  # Mensagem para sair
+while True:  # Loop principal
+    first = input("\nPlase give me a first name: ")  # Pede primeiro nome
+    if first == 'q':  # Sai se digitar 'q'
         break
-    last = input("\nPlase give me a last name: ")
-    if last == 'q':
+    last = input("\nPlase give me a last name: ")  # Pede último nome
+    if last == 'q':  # Sai se digitar 'q'
         break
     
-    formatted_name = get_formatted_name(first, last)
-    print("\tNeatly formatted name : " + formatted_name + ".")
+    formatted_name = get_formatted_name(first, last)  # Formata o nome
+    print("\tNeatly formatted name : " + formatted_name + ".")  
+    # Imprime nome formatado
