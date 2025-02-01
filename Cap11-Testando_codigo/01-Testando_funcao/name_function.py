16605bc7701f9b82691eb808987ca3eeeb2a087d
seperool <seperool@gmail.com>
2025-02-01 20:40:23 -0300

    funcao modificada para nome de 3 partes

--- Cap11-Testando_codigo/01-Testando_funcao/name_function.py
+++ Cap11-Testando_codigo/01-Testando_funcao/name_function.py
@@ -6,7 +6,20 @@ Created on Wed Jan 22 21:55:17 2025
 @author: sergio
 """
 
-def get_formatted_name(first,last):
-    """Gera um nome completo formatado de modo elegante."""
-    full_name = first + " " + last
+def get_formatted_name(first, middle, last):
+    """Gera um nome completo formatado de modo elegante.
+
+    Args:
+        first (str): O primeiro nome.
+        middle (str): O nome do meio.
+        last (str): O sobrenome.
+
+    Returns:
+        str: O nome completo formatado.
+    """
+
+    # Concatena as partes do nome com espaços
+    full_name = first + " " + middle + " " + last
+
+    # Formata o nome para capitalizar a primeira letra de cada palavra
     return full_name.title()
