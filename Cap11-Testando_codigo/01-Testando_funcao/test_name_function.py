8a2ab0c9ed1d0fa86143dd7c021dd714a4947fdb
seperool <seperool@gmail.com>
2025-02-02 17:06:04 -0300

    adicionando novo teste de funcao

--- Cap11-Testando_codigo/01-Testando_funcao/test_name_function.py
+++ Cap11-Testando_codigo/01-Testando_funcao/test_name_function.py
@@ -6,15 +6,21 @@ Created on Sun Jan 26 22:08:15 2025
 @author: sergio
 """
 
-import unittest  # Importa biblioteca de testes
-from name_function import get_formatted_name  # Importa função
+import unittest  # Importa a biblioteca unittest para testes
 
-class NameTestCase(unittest.TestCase):  # Cria classe de teste
-  """Testes para 'name_function.py'"""  # Docstring da classe de teste
+from name_function import get_formatted_name  # Importa a função que será testada
 
-  def test_first_last_name(self):  # Define um método de teste
-    """Nomes como 'janis joplin' funcionam?"""  # Docstring do método de teste
-    formatted_name = get_formatted_name('janis', 'joplin')  # Chama função para formatar nome
-    self.assertEqual(formatted_name, 'Janis Joplin')  # Verifica se o nome formatado está correto
+class NameTestCase(unittest.TestCase):  # Define uma classe para os casos de teste, herdando de unittest.TestCase
+    """Testes para 'name_function.py'"""  # Docstring da classe, descrevendo o propósito dos testes
 
-unittest.main()  # Executa os testes
+    def test_first_last_name(self):  # Define um método de teste para nomes com primeiro e último nome
+        """Nomes como 'janis joplin' funcionam?"""  # Docstring do método, explicando o caso de teste específico
+        formatted_name = get_formatted_name('janis', 'joplin')  # Chama a função com um nome de exemplo
+        self.assertEqual(formatted_name, 'Janis Joplin')  # Verifica se o resultado formatado é o esperado
+
+    def test_first_last_middle_name(self):  # Define um método de teste para nomes com primeiro, do meio e último nome
+        """Nomes como 'Wolfgang Amadeus Mozart' funcionam?"""  # Docstring do método, explicando o caso de teste
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')  # Chama a função com um nome de exemplo
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')  # Verifica se o resultado formatado é o esperado
+
+unittest.main()  # Executa os testes definidos na classe
