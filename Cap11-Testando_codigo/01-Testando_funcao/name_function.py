16b43f721fbf2018ffbc7bbfaedac258c55882d5
seperool <seperool@gmail.com>
2025-02-02 17:05:52 -0300

    respondendo a um teste que falha

--- Cap11-Testando_codigo/01-Testando_funcao/name_function.py
+++ Cap11-Testando_codigo/01-Testando_funcao/name_function.py
@@ -6,20 +6,24 @@ Created on Wed Jan 22 21:55:17 2025
 @author: sergio
 """
 
-def get_formatted_name(first, middle, last):
+def get_formatted_name(first, last, middle=''):
     """Gera um nome completo formatado de modo elegante.
 
     Args:
         first (str): O primeiro nome.
-        middle (str): O nome do meio.
+        middle (str): O nome do meio (opcional).
         last (str): O sobrenome.
 
     Returns:
         str: O nome completo formatado.
     """
-
-    # Concatena as partes do nome com espaços
-    full_name = first + " " + middle + " " + last
+    if middle:
+        # Concatena as partes do nome com espaços, incluindo o nome do meio
+        full_name = first + " " + middle + " " + last
+    else:
+        # Concatena as partes do nome com espaços, sem o nome do meio
+        full_name = first + " " + last
 
     # Formata o nome para capitalizar a primeira letra de cada palavra
     return full_name.title()
+
