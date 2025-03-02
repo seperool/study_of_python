9d192e6ca342224c3a3952a4a0c336ef1941c940
seperool <seperool@gmail.com>
2025-03-01 21:18:46 -0300

    Alterando parametros do grafico

--- Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/mpl_squares.py
+++ Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/mpl_squares.py
@@ -6,8 +6,24 @@ Created on Mon Feb 24 11:14:32 2025
 @author: sergio
 """
 
-import matplotlib.pyplot as plt # Importa a biblioteca matplotlib para criação de gráficos
+# Importa a biblioteca matplotlib para criação de gráficos
+import matplotlib.pyplot as plt
 
-squares = [1,4,9,16,25]         # Cria uma lista de números quadrados
-plt.plot(squares)               # Plota a lista de números quadrados em um gráfico de linha
-plt.show()                      # Exibe o gráfico criado
+
+# Cria uma lista de números quadrados
+squares = [1,4,9,16,25]
+# Cria um gráfico de linha com a lista de números quadrados
+plt.plot(squares,linewidth=5)
+
+# Define o título do gráfico
+plt.title("Square Numbers", fontsize=24)
+# Define o rótulo do eixo x
+plt.xlabel("Value", fontsize=14)
+# Define o rótulo do eixo y
+plt.ylabel("Square of Value", fontsize=14)
+
+# Define o tamanho dos rótulos dos eixos
+plt.tick_params(axis='both', labelsize=14)
+
+# Exibe o gráfico criado
+plt.show()         
