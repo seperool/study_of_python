34a9a0bc2a27e7e13672e9cdd04eaedef49107aa
seperool <seperool@gmail.com>
2025-03-06 21:00:03 -0300

    Grafico de ponto

---
+++ Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/grafico_ponto/scatter_squares.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  6 20:50:52 2025
+
+@author: sergio
+"""
+
+# Importa a biblioteca matplotlib para criar gráficos
+import matplotlib.pyplot as plt
+
+# Cria um gráfico de dispersão com um ponto na coordenada (2, 4)
+plt.scatter(2,4)
+# Adiciona uma grade ao gráfico
+plt.grid()
+# Exibe o gráfico
+plt.show()
