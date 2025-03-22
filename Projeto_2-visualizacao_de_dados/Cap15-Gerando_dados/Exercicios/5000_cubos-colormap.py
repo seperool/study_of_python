a8634976c2e57afb7a9c0c2058df93ab7d9749da
seperool <seperool@gmail.com>
2025-03-21 21:55:41 -0300

    Colormap - Exercicios

--- Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/Exercicios/5000_cubos-colormap.py
+++ Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/Exercicios/5000_cubos-colormap.py
@@ -10,24 +10,15 @@ Created on Mon Mar  3 18:29:56 2025
 import matplotlib.pyplot as plt
 
 # Lista de valores do eixo x
-input_values = []
-i=0
-while i < 5000:
-    i = i + 1
-    input_values.append(i)
-    #print(i)
+input_values = list(range(1,5001))
 # Cria uma lista de números ao cubo
 # Lista de valores do eixo y
-cubos = []
-
-for value in input_values:
-    cubo = value**3
-    cubos.append(cubo)
+cubos = [value**3 for value in input_values]
 
 # Plotagem
 
 # Cria um gráfico de linha com a lista de números quadrados
-plt.plot(input_values, cubos, c=cubos, cmap=plt.cm.Blues, 
+plt.scatter(input_values, cubos, c=cubos, cmap=plt.cm.Blues, 
          edgecolor='none', s=40)
 # Especifica o intervalo dos eixos
 plt.axis([0,5000,0,1e11])
@@ -47,4 +38,4 @@ plt.show()
 
 # Salva o gráfico de maneira automática
 # Argumentos: nome do gráfico e remove espaços em branco
-#plt.savefig("plot_cubos.png", bbox_inches='tight')
+plt.savefig("plot_cubos5000-colormap.png", bbox_inches='tight')
--- Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/Exercicios/5_cubos-colormap.py
+++ Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/Exercicios/5_cubos-colormap.py
@@ -10,17 +10,17 @@ Created on Mon Mar  3 18:25:50 2025
 import matplotlib.pyplot as plt
 
 # Lista de valores do eixo x
-input_values = [1,2,3,4,5]
+x = list(range(1,6))
+
 # Cria uma lista de números ao cubo
 # Lista de valores do eixo y
-cubos = [value**3 for value in input_values]
-print(cubos)
+y = [value**3 for value in x]
+print(y)
 
 # Plotagem
 
 # Cria um gráfico de linha com a lista de números quadrados
-plt.plot(input_values, cubos, c=cubos, cmap=plt.cm.Blues, 
-         edgecolor='none')
+plt.scatter(x, y, c=y, cmap=plt.cm.Blues, edgecolor='none', s=40)
 
 # Define o título do gráfico
 plt.title("Square Numbers", fontsize=24)
@@ -37,4 +37,4 @@ plt.show()
 
 # Salva o gráfico de maneira automática
 # Argumentos: nome do gráfico e remove espaços em branco
-plt.savefig("plot_cubo.png", bbox_inches='tight')
+plt.savefig("plot_cubo5-colormap.png", bbox_inches='tight')
