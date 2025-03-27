85d9070b50a454c40f5f6d4f2454e8ccd936491f
seperool <seperool@gmail.com>
2025-03-27 11:52:13 -0300

    plotando randomwalk

---
+++ Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/02-Passeios_aleatorios/rw_visual.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 26 16:50:12 2025
+
+@author: sergio
+"""
+
+# Importando biblioteca
+import matplotlib.pyplot as plt
+
+# Importando classe
+from random_walk import RandomWalk
+
+# Criando instância
+rw = RandomWalk()
+# Definindo passeio aleatório
+rw.fill_walk()
+
+#Plotando gráfico de distribuição
+plt.scatter(rw.x_values, rw.y_values, s=15)
+plt.show()
+
+# Salvando gráfico
+plt.savefig('rw_scatter.png', bbox_inches='tight')
