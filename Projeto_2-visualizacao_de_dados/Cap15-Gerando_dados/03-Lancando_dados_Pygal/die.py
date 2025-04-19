226ee337d8c40eb50c6f4401d27ff1d4e525ad3c
seperool <seperool@gmail.com>
2025-04-19 18:14:08 -0300

    classe die - dado

---
+++ Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/03-Lancando_dados_Pygal/die.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 19 18:09:47 2025
+
+@author: sergio
+"""
+
+from random import randint
+
+class Die():
+    """Uma classe que representa um único dado."""
+    
+    def __init__(self, num_sides=6):
+        """Supõe que seja um dado de seis lados"""
+        self.num_sides=num_sides
+        
+        def roll(self):
+            """Devolve um valor aleatório entre 1 e o número de lados"""
+            return randint(1, self.num_sides)
