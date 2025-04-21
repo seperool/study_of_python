17e2fb2b2d97537fc93522c297cc8bbbd371b51d
seperool <seperool@gmail.com>
2025-04-21 13:24:47 -0300

    Classe atualizada

--- Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/03-Lancando_dados_Pygal/die.py
+++ Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/03-Lancando_dados_Pygal/die.py
@@ -15,6 +15,6 @@ class Die():
         """Supõe que seja um dado de seis lados"""
         self.num_sides=num_sides
         
-        def roll(self):
-            """Devolve um valor aleatório entre 1 e o número de lados"""
-            return randint(1, self.num_sides)
+    def roll(self):
+        """Devolve um valor aleatório entre 1 e o número de lados"""
+        return randint(1, self.num_sides)
