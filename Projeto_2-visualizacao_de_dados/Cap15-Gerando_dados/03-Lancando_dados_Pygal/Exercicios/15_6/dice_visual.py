412e6dc692602c0a2b62d59ee5f56d8d5869a521
seperool <seperool@gmail.com>
2025-05-05 10:52:22 -0300

    Exercicios - lançamento de dados - Gráficos em pygal

---
+++ Projeto_2-visualizacao_de_dados/Cap15-Gerando_dados/03-Lancando_dados_Pygal/Exercicios/15_6/dice_visual.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 30 13:51:02 2025
+
+@author: sergio
+"""
+
+# Importando bibliotecas
+# Importa a classe Die do módulo die
+from die import Die
+# Importa a biblioteca pygal para gráficos.
+import pygal
+
+# Cria um dado de 6 lados, instância
+D6_1 = Die()
+D6_2 = Die()
+
+# Inicializa uma lista para armazenar os resultados
+results = []
+# Simula 1000 lançamentos
+for roll_num in range(1000):
+    # Lança o dado
+    result = D6_1.roll()+D6_2.roll()
+    # Adiciona o resultado à lista
+    results.append(result)
+
+# Imprime os resultados
+print(results)
+
+# Inicializa uma lista para armazenar as frequências
+frequencies = []
+max_result = D6_1.num_sides + D6_2.num_sides
+# Calcula a frequência de cada valor
+for value in range(2, max_result + 1):
+    # Conta as ocorrências do valor
+    frequency = results.count(value)
+    # Adiciona a frequência à lista
+    frequencies.append(frequency)
+
+# Imprime as frequências
+print(frequencies)
+
+# Visualiza os resultados
+# Cria um gráfico de barras.
+hist = pygal.Bar()
+# Define o título.
+hist.title = "Results of rolling two D6 dice 1000 times."
+# Define os rótulos do eixo x.
+hist.x_labels = [value for value in range(2,max_result + 1)]
+# Define o título do eixo x.
+hist.x_title = "Result"
+# Define o título do eixo y.
+hist.y_title = "Frequency of Result"
+# Adiciona os dados ao gráfico.
+hist.add('D6 + D6', frequencies)
+
+# Salva o gráfico em SVG.
+hist.render_to_file("dice_visual.svg")
+# Informa que o gráfico foi salvo.
+print("Gráfico salvo como dice_visual.svg")
