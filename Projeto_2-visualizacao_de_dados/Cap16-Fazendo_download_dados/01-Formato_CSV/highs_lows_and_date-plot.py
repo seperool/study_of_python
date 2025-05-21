096634b00fd874eaac592f8de0764b24db2db945
seperool <seperool@gmail.com>
2025-05-21 11:01:38 -0300

    Programa - data e maxima temperatura

---
+++ Projeto_2-visualizacao_de_dados/Cap16-Fazendo_download_dados/01-Formato_CSV/highs_lows_and_date-plot.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 21 10:52:10 2025
+
+@author: sergio
+"""
+
+# Importa o módulo csv para trabalhar com arquivos CSV.
+import csv
+from datetime import datetime
+# Importa funções do matplotlib para plotagem.
+from matplotlib import pyplot as plt
+
+#Obtém as datas e as temperaturas máximas do arquivo
+
+# Define o nome do arquivo CSV a ser lido.
+filename = 'sitka_weather_07-2014.csv'
+# Abre o arquivo CSV.
+with open(filename) as f:
+    # Cria um objeto para ler o arquivo CSV.
+    reader = csv.reader(f)
+    # Lê a primeira linha, contendo os cabeçalhos.
+    header_row = next(reader)
+
+    # Lista para armazenar as temperaturas máximas.
+    dates, highs = [], []
+    # Loop pelas linhas restantes do arquivo.
+    for row in reader:
+        
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+        
+        # Converte o valor da segunda coluna para inteiro (temperatura máxima).
+        high = int(row[1])
+        # Adiciona a temperatura máxima à lista.
+        highs.append(high)
+
+# Cria a figura para o gráfico.
+fig = plt.figure(dpi=128,figsize=(10,6))
+# Plota as temperaturas máximas em vermelho.
+plt.plot(dates, highs, c='red')
+
+# Define o título do gráfico e os rótulos dos eixos.
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+# Define o rótulo do eixo x.
+plt.xlabel("",fontsize=16)
+fig.autofmt_xdate()
+# Define o rótulo do eixo y.
+plt.ylabel("Temperature (F)",fontsize=16)
+# Define o tamanho dos rótulos dos eixos.
+plt.tick_params(axis='both',which='major',labelsize=16)
+
+# Exibe o gráfico.
+plt.show()
